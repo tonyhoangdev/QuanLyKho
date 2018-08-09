1ab719e3f896b7a5f53ee9e87f842a38f48b9ac4
Tony Hoang <hoangtrongminh304@gmail.com>
2018-08-10 02:37:50 +0700

    tmp add loaded

--- QuanLyKho/ViewModel/MainViewModel.cs
+++ QuanLyKho/ViewModel/MainViewModel.cs
@@ -4,21 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Input;
 
 namespace QuanLyKho.ViewModel
 {
     public class MainViewModel : BaseViewModel
     {
         public bool IsLoaded = false;
+        public ICommand LoadedWindowCommand { get; set; }
 
         public MainViewModel()
         {
-            if (!IsLoaded)
+            LoadedWindowCommand = new RelayCommand<object>((p) => { return true; }, (p) =>
             {
                 IsLoaded = true;
                 LoginWindow loginWindow = new LoginWindow();
                 loginWindow.ShowDialog();
-            }
+            });
         }
     }
 }
